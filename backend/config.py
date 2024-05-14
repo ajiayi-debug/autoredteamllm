ff97f4e052f3cd3cb9709918b49e8d17cd170946
ajiayi-debug <e0774928@u.nus.edu>
2024-05-14 11:42:41 +0000

    .

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,5 @@ uri = "mongodb+srv://iyaiji:iyaiji@blueteam.zxnijed.mongodb.net/mydatabase"
 #Choose your model type according to whatever is available in groq. 
 groq_model="llama3-70b-8192"
 groq_api='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'
-huggingface_model= "meta-llama/Meta-Llama-3-8B"
+huggingface_model= "meta-llama/Meta-Llama-3-8B-instruct"
+huggingface_authentication = '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'
