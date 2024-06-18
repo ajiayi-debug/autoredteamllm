9f122369f40e216541bfef67f39fd27cb224b3cb
ajiayi-debug <e0774928@u.nus.edu@domain.com>
2024-06-18 21:47:17 +0800

    git ignore

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-#Create your own cluster on mongodb, wither through your local machine or using the website https://www.mongodb.com/ (i used the website and connected it to vscode for easier access)
+#Create your own cluster on mongodb, whether through your local machine or using the website https://www.mongodb.com/ (i used the website and connected it to vscode for easier access)
 uri = "mongodb+srv://iyaiji:iyaiji@blueteam.zxnijed.mongodb.net/mydatabase"
 #Choose your model type according to whatever is available in groq. 
 groq_model="llama3-70b-8192"
