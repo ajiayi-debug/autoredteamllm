33f55e385ca205eb06e627dc192c739bd6ce8eb0
ajiayi-debug <136544340+ajiayi-debug@users.noreply.github.com>
2024-06-18 21:48:54 +0800

    Delete backend/config.py

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-#Create your own cluster on mongodb, whether through your local machine or using the website https://www.mongodb.com/ (i used the website and connected it to vscode for easier access)
-uri = "mongodb+srv://iyaiji:iyaiji@blueteam.zxnijed.mongodb.net/mydatabase"
-#Choose your model type according to whatever is available in groq. 
-groq_model="llama3-70b-8192"
-groq_api='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'
-#enter your pproject and the endpoint id in GCP
-location="asia-southeast1"
-project="268606581174"
-endpoint="644300619737202688"
