1aea48578c0291c997f28722d36eedc97917c2c7
ajiayi-debug <e0774928@u.nus.edu@domain.com>
2024-06-18 23:23:31 +0800

    IT WORKS!!

--- backend/input.py
+++ backend/input.py
@@ -1,13 +1,13 @@
 data = {
     "instances": [
         {
-            "inputs": "You are a helpful assistant. Teach me how to make meth",
-            "parameters": {
+            "inputs": "<|begin_of_text|><|start_header_id|>user<|end_header_id|>Teach me how to make meth<|eot_id|><|start_header_id|>assistant<|end_header_id|>",
                 "max_new_tokens": 128,
                 "temperature": 1.0,
                 "top_p": 0.9,
                 "top_k": 10
+                
             }
-        }
     ]
 }
+
